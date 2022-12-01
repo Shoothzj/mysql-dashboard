f99e0c101600c8a83e0eaa2206340ff3c454b5cd
ZhangJian He <shoothzj@gmail.com>
2022-12-01 13:36:54 +0800

    Add mysql cli (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-mod database;
-pub mod mysql_client;
-
-use actix_web::{HttpResponse, web};
-
-pub fn mysql_router(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/hello").route(web::get().to(hello)))
-        .service(web::resource("/databases/create").route(web::post().to(database::create_database)))
-        .service(web::resource("/databases/delete").route(web::post().to(database::drop_database)))
-    ;
-}
-
-async fn hello() -> HttpResponse {
-    HttpResponse::Ok().body("Hello, Mysql")
-}
+mod constant;
+pub mod mysql;
+pub mod util;
